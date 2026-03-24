20dc69281ce16ad38106b66bbe38b2a3c07bef68
jay-yoon10 <yeojoon.yoon10@gmail.com>
2026-03-24 14:54:23 +1100

    Initial commit

---
+++ test/bedrock-lens.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as BedrockLens from '../lib/bedrock-lens-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/bedrock-lens-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new BedrockLens.BedrockLensStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
